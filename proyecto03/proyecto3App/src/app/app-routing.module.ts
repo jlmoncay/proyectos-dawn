d993632be8923f75d62a322934582eb6d758a0d4
jlmoncay <jlmoncay@espol.edu.ec>
2023-01-03 14:44:45 -0500

    Página de inicio (imagen, titulo y botón)

--- proyecto03/proyecto3App/src/app/app-routing.module.ts
+++ proyecto03/proyecto3App/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { CategoriaComponent } from './categoria/categoria.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "categoria", component: CategoriaComponent },
+  { path: "inicio", component: InicioComponent },
+  { path: "**", redirectTo: "inicio" }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
